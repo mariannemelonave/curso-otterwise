91b8b55362aa97b5c08ff05174f80c3101de3077
mariannemelonave <106995156+mariannemelonave@users.noreply.github.com>
2022-08-25 17:25:36 -0300

    estudo de caso semana 13

--- semana13/exemplo-prisma/src/config/server.js
+++ semana13/exemplo-prisma/src/config/server.js
@@ -1,11 +1,30 @@
 import Fastify from "fastify";
 import postsRoutes from "../routes/posts-routes.js";
+import multer from "fastify-multer";
+import fastifyStatic from "@fastify/static"
+import path from "path"
+import cors from "@fastify/cors"
 
 const fastify = Fastify({
     logger: true,
 });
 
+const __dirname = path.resolve();
 
-fastify.register(postsRoutes)
+fastify.register(multer.contentParser);
+
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, "pubic"),
+    prefix: "/public/"
+})
+
+fastify.register(postsRoutes);
+
+fastify.register(cors, {
+    origin: (origin, callback) => {
+        callback(null, true);
+        return;
+    }
+});
 
 export default fastify;
